0aab0e5c459080d0d10cc731c3866c876b57a17f
chaitalikadam <chaitalikadam25@gmail.com>
2021-10-10 23:11:46 +0530

    first commit

---
+++ Add.py
@@ -0,0 +1,11 @@
+# PYthon program to add 2 numbers
+
+number1 = 30
+number2 = 15
+
+#Operation to add two numbers
+
+res = number1 + number2
+
+#Display result
+print("Sum of {0} and {1} is {2}".format(number1, number2, res))
